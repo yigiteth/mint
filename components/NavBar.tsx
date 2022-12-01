021008badda267a75e8a0d0e71b88d696f1b2a6a
yigit.eth <iamdexmedia@gmail.com>
2022-12-01 14:50:09 +0300

    solana bootcamp

---
+++ components/NavBar.tsx
@@ -0,0 +1,17 @@
+import { HStack, Spacer } from '@chakra-ui/react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { FC } from 'react';
+import styles from '../styles/Home.module.css';
+
+import React from 'react';
+
+const NavBar: FC = () => {
+  return (
+    <HStack width='full' padding={4}>
+      <Spacer />
+      <WalletMultiButton className={styles['wallet-adapter-button-trigger']} />
+    </HStack>
+  );
+};
+
+export default NavBar;
---
+++ pages/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { Box, Center, Spacer, Stack } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Disconnected from '../components/Disconnected';
+import Connected from '../components/Connected';
+import NavBar from '../components/NavBar';
+import styles from '../styles/Home.module.css';
+import { useWallet } from '@solana/wallet-adapter-react';
+
+const Home: NextPage = () => {
+  const { connected } = useWallet();
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Buildoors</title>
+        <meta name='The NFT Collection for Buildoors' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <Box
+        w='full'
+        h='calc(100vh)'
+        bgImage={'url(/home-background.svg)'}
+        backgroundPosition='center'>
+        <Stack w='full' h='calc(100vh)' justify='center'>
+          <NavBar />
+
+          <Spacer />
+          <Center>
+            {/* If connected, the second view, otherwise the first */}
+            <Center>{connected ? <Connected /> : <Disconnected />}</Center>
+          </Center>
+          <Spacer />
+
+          <Center>
+            <Box marginBottom={4} color='white'>
+              <a
+                href='https://twitter.com/_buildspace'
+                target='_blank'
+                rel='noopener noreferrer'>
+                built with @_buildspace
+              </a>
+            </Box>
+          </Center>
+        </Stack>
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
